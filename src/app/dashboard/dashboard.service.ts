e3bcb073c99d3d7377d059d95e2a2f7cc85af164
Hanish <akhanish7@gmail.com>
2024-06-15 16:25:48 +0530

    Added filter params for api call

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 
 @Injectable({
   providedIn : 'root'
@@ -9,12 +9,19 @@ export class DashboardService{
   baseUrl;
   constructor(private httpClient : HttpClient) {
     //this.baseUrl = 'https://movie-system-lbrl.onrender.com'
-    this.baseUrl = 'http:/localhost:4000'
+    this.baseUrl = 'http://localhost:4000'
   }
 
   getTopMovies(filters : any){
-    console.log(this.baseUrl);
-    return this.httpClient.get(`http:/localhost:4000/movie/top`);
+    let params = new HttpParams();
+
+    for (let key in filters) {
+      if (filters[key] != null && filters[key] !== undefined) {
+        params = params.set(key, filters[key]);
+      }
+    }
+
+    return this.httpClient.get(`${this.baseUrl}/movie/top`, { params });
   }
 
 }
