bb563be6a3a419008870e40ed15e05cd2ce7bde0
Hanish <akhanish7@gmail.com>
2024-06-16 03:06:58 +0530

    Added Google Login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,14 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import {RouterModule} from "@angular/router";
 import {AuthService} from "./auth.service";
-import { GoogleLoginComponent } from './google-login/google-login.component';
-
 
 
 @NgModule({
   declarations: [
     AuthComponent,
-    GoogleLoginComponent
   ],
   imports: [
     CommonModule,
